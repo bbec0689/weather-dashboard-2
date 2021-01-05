b16702bee027bb447759a32c04af70ca45013124
Dave <bbec0689@gmail.com>
2021-01-04 19:32:27 -0700

    fixed my html/bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,22 @@ var getCityWS = document.querySelector("#city-ws")
 var getCityHumid = document.querySelector("#city-humid")
 
 
+// Current City Weather
+
 function getWeather() {
     fetch('https://api.openweathermap.org/data/2.5/weather?q=Toronto,CA&appid=64a50eda68eb7a2b92c0bc02b65b4593')
 
-    .then(function (response) {
-        return response.json();
-    })
+        .then(function (response) {
+            return response.json();
+        })
 
-    .then(function (data) {
-        getCity(data);
-        getTemp(data);
-        getWS(data);
-        getHumid(data)
-        console.log(data)
-    })
-
-    
+        .then(function (data) {
+            getCity(data);
+            getTemp(data);
+            getWS(data);
+            getHumid(data)
+            console.log(data)
+        })
 }
 
 function getCity(data) {
@@ -46,3 +46,20 @@ function getWS(data) {
     var cityWSText = document.createTextNode(cityWS)
     getCityWS.appendChild(cityWSText);
 }
+
+//5 Day Forecast
+
+function getForecast(cityId) {
+    fetch(
+        "https://api.openweathermap.org/data/2.5/forecast?id=" + cityId + '&appid=64a50eda68eb7a2b92c0bc02b65b4593'
+    )
+
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (data) {
+            
+        })
+}
+
